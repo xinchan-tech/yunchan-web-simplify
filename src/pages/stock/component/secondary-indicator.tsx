563b1deb1ff371d0156015ee63d52ca3d7ca3f5e
Sieunyue <sieunyue@gmail.com>
2025-01-02 10:40:20 +0800

    feat: 添加指标参数编辑功能，优化二级指标组件逻辑

--- src/pages/stock/component/secondary-indicator.tsx
+++ src/pages/stock/component/secondary-indicator.tsx
@@ -1,8 +1,10 @@
-import { Input, JknIcon, Label, Popover, PopoverContent, PopoverTrigger, RadioGroup, RadioGroupItem, ScrollArea } from "@/components"
+import { Input, JknIcon, Label, Popover, PopoverContent, PopoverTrigger, RadioGroup, RadioGroupItem, ScrollArea, useModal } from "@/components"
 import { getStockIndicators } from "@/api"
 import { useQuery } from "@tanstack/react-query"
 import { useState } from "react"
 import { useKChartContext } from "../lib"
+import { useIndicator } from "@/store"
+import { useMount } from "ahooks"
 
 interface SecondaryIndicatorProps {
   /**
@@ -69,45 +71,91 @@ export const SecondaryIndicator = (props: SecondaryIndicatorProps) => {
     }
   })()
 
-  return (
-    <Popover>
-      <PopoverTrigger className="" asChild>
-        <div className="px-2 py-1 rounded-sm hover:text-secondary cursor-pointer hover:border-dialog-border left-2 top-0 border border-solid border-border text-sm text-tertiary">
-          <span>{name}</span>
-          <JknIcon name="arrow_down" className="w-3 h-3 ml-1" />
-        </div>
-      </PopoverTrigger>
-      <PopoverContent align="start" side="top" sideOffset={10} alignOffset={-10} className="w-fit p-0">
-        <div className="text-sm">
-          <Input placeholder="搜索指标" className="border-none placeholder:text-tertiary" value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
-          <div className="flex">
-            {
-              list?.map((item) => (
-                <div key={item.id} className="flex flex-col w-48">
-                  <div className="border-0 border-b border-t border-solid border-border text-center py-1 bg-background">{item.name}</div>
-                  <ScrollArea className="h-[300px]">
-                    <RadioGroup value={currentSecondaryIndicator.id} onValueChange={_onChange}>
-                      {item.indicators.map((ele) => (
-                        <div
-                          className="hover:bg-primary cursor-pointer px-2 flex items-center w-full"
-                          key={ele.id}
-                          onKeyDown={() => { }}
-                        >
-                          <RadioGroupItem value={ele.id} id={`stock-secondary-indicator-${props.mainIndex}-${props.index}-${ele.id}`} />
-                          <Label className="ml-2 flex-1 py-3" htmlFor={`stock-secondary-indicator-${props.mainIndex}-${props.index}-${ele.id}`}>
-                            {ele.name}
-                          </Label>
-                        </div>
-                      ))}
+  const indicatorParamsForm = useModal({
+    content: <IndicatorParamsForm />,
+    title: '指标参数编辑',
+  })
 
-                    </RadioGroup>
-                  </ScrollArea>
+  useMount(() => {
+    indicatorParamsForm.modal.open()
+  })
+
+  return (
+    <div>
+      {
+        indicatorParamsForm.context
+      }
+      <Popover>
+        <PopoverTrigger className="" asChild>
+          <div className="px-2 py-1 rounded-sm hover:text-secondary cursor-pointer hover:border-dialog-border left-2 top-0 border border-solid border-border text-sm text-tertiary">
+            <span>{name}</span>
+            <JknIcon name="arrow_down" className="w-3 h-3 ml-1" />
+          </div>
+        </PopoverTrigger>
+        <PopoverContent align="start" side="top" sideOffset={10} alignOffset={-10} className="w-fit p-0">
+          <div className="text-sm">
+            <div className="flex items-center pr-2">
+              <div className="flex-1">
+                <Input placeholder="搜索指标" className="border-none placeholder:text-tertiary" value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
+              </div>
+              <JknIcon name="ic_settings" className="w-4 h-4 cursor-pointer" />
+            </div>
+            <div className="flex">
+              {
+                list?.map((item) => (
+                  <div key={item.id} className="flex flex-col w-48">
+                    <div className="border-0 border-b border-t border-solid border-border text-center py-1 bg-background">{item.name}</div>
+                    <ScrollArea className="h-[300px]">
+                      <RadioGroup value={currentSecondaryIndicator.id} onValueChange={_onChange}>
+                        {item.indicators.map((ele) => (
+                          <div
+                            className="hover:bg-primary cursor-pointer px-2 flex items-center w-full"
+                            key={ele.id}
+                            onKeyDown={() => { }}
+                          >
+                            <RadioGroupItem value={ele.id} id={`stock-secondary-indicator-${props.mainIndex}-${props.index}-${ele.id}`} />
+                            <Label className="ml-2 flex-1 py-3" htmlFor={`stock-secondary-indicator-${props.mainIndex}-${props.index}-${ele.id}`}>
+                              {ele.name}
+                            </Label>
+                          </div>
+                        ))}
+
+                      </RadioGroup>
+                    </ScrollArea>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        </PopoverContent>
+      </Popover>
+    </div>
+  )
+}
+
+
+const IndicatorParamsForm = () => {
+  const { indicatorParams } = useIndicator()
+
+  return (
+    <div>
+      {
+        indicatorParams.map((item) => (
+          <div key={item.id}>
+            <div>{item.id}</div>
+            {
+              item.params.map((ele) => (
+                <div key={ele.name}>
+                  <div>{ele.name}</div>
+                  <div>{ele.value}</div>
+                  <div>{ele.min}</div>
+                  <div>{ele.max}</div>
                 </div>
               ))
             }
           </div>
-        </div>
-      </PopoverContent>
-    </Popover>
+        ))
+      }
+    </div>
   )
 }
---
+++ src/store/indicator.ts
@@ -0,0 +1,280 @@
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+type IndicatorParams = {
+  id: string | number
+  name: string
+  params: {
+    name: string
+    value: string
+    max?: string
+    min?: string
+  }[]
+}
+
+const defaultIndicatorParams: IndicatorParams[] = [
+  {
+    id: '241',
+    name: '黄蓝梯子(稳健版)',
+    params: [
+      {
+        name: '蓝色梯子',
+        value: '36',
+        min: '1',
+        max: '1000'
+      },
+      {
+        name: '黄色梯子',
+        value: '135',
+        min: '1',
+        max: '1000'
+      }
+    ]
+  },
+  {
+    id: '242',
+    name: '黄蓝梯子(极速版)',
+    params: [
+      {
+        name: '蓝色梯子',
+        value: '24',
+        min: '1',
+        max: '1000'
+      },
+      {
+        name: '黄色梯子',
+        value: '90',
+        min: '1',
+        max: '1000'
+      }
+    ]
+  },
+  {
+    id: '243',
+    name: 'MA系统',
+    params: [
+      { name: 'P1', value: '3', min: '0', max: '1000' },
+      { name: 'P2', value: '5', min: '0', max: '1000' },
+      { name: 'P3', value: '10', min: '0', max: '1000' },
+      { name: 'P4', value: '20', min: '0', max: '1000' },
+      { name: 'P5', value: '30', min: '0', max: '1000' },
+      { name: 'P6', value: '60', min: '0', max: '1000' },
+      { name: 'P7', value: '90', min: '0', max: '1000' },
+      { name: 'P8', value: '100', min: '0', max: '1000' },
+      { name: 'P9', value: '120', min: '0', max: '1000' },
+      { name: 'P10', value: '250', min: '0', max: '1000' },
+      { name: 'P11', value: '0', min: '0', max: '1000' },
+      { name: 'P12', value: '0', min: '0', max: '1000' },
+      { name: 'P13', value: '0', min: '0', max: '1000' },
+      { name: 'P14', value: '0', min: '0', max: '1000' },
+      { name: 'P15', value: '0', min: '0', max: '1000' }
+    ]
+  },
+  {
+    id: '127',
+    name: '神奇九转',
+    params: [{ name: 'N', value: '15', min: '0', max: '1000' }]
+  },
+  {
+    id: '122',
+    name: 'BOLL',
+    params: [
+      { name: 'SD', value: '120', min: '2', max: '120' },
+      { name: 'WIDTH', value: '2', min: '0', max: '100' }
+    ]
+  },
+  {
+    id: '126',
+    name: 'KC',
+    params: [
+      { name: 'P1', value: '20', min: '1', max: '1000' },
+      { name: 'M', value: '14', min: '1', max: '1000' }
+    ]
+  },
+  {
+    id: '123',
+    name: 'SAR',
+    params: [
+      { name: 'P1', value: '4', min: '0', max: '100' },
+      { name: 'P2', value: '2', min: '0', max: '100' },
+      { name: 'P3', value: '20', min: '0', max: '1000' }
+    ]
+  },
+  {
+    id: '125',
+    name: 'IC',
+    params: [
+      { name: 'LONG', value: '44', min: '1', max: '300' },
+      { name: 'MID', value: '22', min: '1', max: '300' },
+      { name: 'SHORT', value: '7', min: '1', max: '300' }
+    ]
+  },
+  {
+    id: '219',
+    name: 'AI大趋势',
+    params: [
+      { name: 'SHORT', value: '20', min: '2', max: '1000' },
+      { name: 'LONG', value: '120', min: '2', max: '1000' },
+      { name: 'MID', value: '60', min: '2', max: '1000' }
+    ]
+  },
+  {
+    id: '111',
+    name: 'MACD',
+    params: [
+      { name: 'SHORT', value: '12', min: '2', max: '200' },
+      { name: 'LONG', value: '26', min: '2', max: '200' },
+      { name: 'M', value: '9', min: '2', max: '200' }
+    ]
+  },
+  {
+    id: '134',
+    name: 'KDJ',
+    params: [
+      { name: 'P1', value: '9', min: '2', max: '90' },
+      { name: 'P2', value: '3', min: '2', max: '30' },
+      { name: 'P3', value: '3', min: '2', max: '30' }
+    ]
+  },
+  {
+    id: '135',
+    name: 'RSI',
+    params: [
+      { name: 'P1', value: '6', min: '2', max: '120' },
+      { name: 'P2', value: '12', min: '2', max: '250' },
+      { name: 'P3', value: '24', min: '2', max: '500' }
+    ]
+  },
+  {
+    id: '141',
+    name: 'CCI',
+    params: [{ name: 'N', value: '14', min: '2', max: '100' }]
+  },
+  {
+    id: '130',
+    name: 'WWSR',
+    params: [{ name: 'N', value: '14', min: '2', max: '100' }]
+  },
+  {
+    id: '145',
+    name: 'PSY',
+    params: [
+      { name: 'N', value: '12', min: '2', max: '100' },
+      { name: 'M', value: '6', min: '2', max: '100' }
+    ]
+  },
+  {
+    id: '140',
+    name: 'BIAS',
+    params: [
+      { name: 'N1', value: '6', min: '2', max: '250' },
+      { name: 'N2', value: '12', min: '2', max: '250' },
+      { name: 'N3', value: '24', min: '2', max: '250' }
+    ]
+  },
+  {
+    id: '138',
+    name: 'DMA',
+    params: [
+      { name: 'SHORT', value: '10', min: '2', max: '60' },
+      { name: 'LONG', value: '50', min: '2', max: '250' },
+      { name: 'M', value: '10', min: '2', max: '100' }
+    ]
+  },
+  {
+    id: '142',
+    name: 'DMI',
+    params: [
+      { name: 'N', value: '14', min: '2', max: '90' },
+      { name: 'M', value: '6', min: '2', max: '60' }
+    ]
+  },
+  {
+    id: '143',
+    name: 'MTM',
+    params: [
+      { name: 'N', value: '12', min: '2', max: '120' },
+      { name: 'M', value: '6', min: '2', max: '60' }
+    ]
+  },
+  {
+    id: '144',
+    name: 'OSC',
+    params: [
+      { name: 'N', value: '20', min: '2', max: '100' },
+      { name: 'M', value: '6', min: '2', max: '60' }
+    ]
+  },
+  {
+    id: '146',
+    name: 'VR',
+    params: [
+      { name: 'N', value: '26', min: '2', max: '100' },
+      { name: 'M', value: '6', min: '2', max: '100' }
+    ]
+  },
+  {
+    id: '147',
+    name: 'ARBR',
+    params: [{ name: 'N', value: '26', min: '2', max: '120' }]
+  },
+  {
+    id: '137',
+    name: 'CR',
+    params: [
+      { name: 'N', value: '26', min: '2', max: '100' },
+      { name: 'M1', value: '5', min: '0', max: '100' },
+      { name: 'M2', value: '10', min: '0', max: '100' },
+      { name: 'M3', value: '20', min: '0', max: '100' },
+      { name: 'M4', value: '60', min: '0', max: '100' },
+      { name: 'M', value: '6', min: '0', max: '100' }
+    ]
+  },
+  {
+    id: '139',
+    name: 'EMV',
+    params: [
+      { name: 'N', value: '14', min: '2', max: '90' },
+      { name: 'N1', value: '9', min: '2', max: '60' }
+    ]
+  },
+  {
+    id: '132',
+    name: 'MAVOL',
+    params: [
+      { name: 'N1', value: '5', min: '0', max: '1000' },
+      { name: 'N2', value: '10', min: '0', max: '1000' },
+      { name: 'N3', value: '20', min: '0', max: '1000' }
+    ]
+  }
+]
+
+interface IndicatorStore {
+  indicatorParams: IndicatorParams[]
+  setIndicatorParams: (params: IndicatorParams[]) => void
+  isDefaultIndicatorParams: (indicatorId: string) => boolean
+}
+
+export const useIndicator = create<IndicatorStore>()(
+  persist(
+    (set, get) => ({
+      indicatorParams: defaultIndicatorParams,
+      setIndicatorParams: params => set({ indicatorParams: params }),
+      isDefaultIndicatorParams: indicatorId => {
+        const indicator = defaultIndicatorParams.find(item => item.id === indicatorId)
+
+        if(!indicator) {
+          return false
+        }
+
+        return JSON.stringify(indicator) === JSON.stringify(
+          get().indicatorParams.find(item => item.id === indicatorId)
+        )
+      }
+    }),
+    {
+      name: 'indicator',
+      storage: createJSONStorage(() => localStorage)
+    }
+  )
+)
