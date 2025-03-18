23809f8459a50580dcb2c94c6d2d43da14f9c125
leiyaojun <leiyaojun.one@aliyun.com>
2025-03-18 19:00:04 +0800

    feat: 调整字体引入方式

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      pingfang: ['PingFang SC', 'sans-serif']
+    },
     extend: {
       textColor: {
         secondary: 'hsl(var(--text-secondary))',
